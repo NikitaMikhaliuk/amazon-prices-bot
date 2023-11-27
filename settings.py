1b79aee7e24a9252562607f64339c194cec87cbb
Nikita Mikhaliuk <nikkita1992@mail.ru>
2023-11-27 22:28:39 +0300

    add: settings implementation

---
+++ settings.py
@@ -0,0 +1,14 @@
+from pydantic import Field, SecretStr
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    bot_token: SecretStr = Field(default=...)
+    api_host: str = Field(default=...)
+    api_key: str = Field(default=...)
+    py_env: str = Field(default="PRODUCTION")
+
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
+
+config = Settings()
