050d2551c5b5703dc04f0807627cd531fdb2a088
Nikita Mikhaliuk <nikkita1992@mail.ru>
2023-12-04 21:07:32 +0300

    add: debugging logic

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import asyncio
 import logging
 import os
 
+from icecream import install
+
 import database as db
 from api import api
 from settings import config
@@ -9,6 +11,7 @@ from telegram.bot import start_bot
 
 
 def main():
+    install()
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
